6b7cab60219084731633306d085921c24dc97121
Kerry Guarino <kerry.guarino@gmail.com>
2023-10-16 19:45:56 -0400

    completed lab

--- lib/app.py
+++ lib/app.py
@@ -1,1 +1,11 @@
-print("Hello World! Pass this test, please.")
+print("Hello World! Pass this test, please.", end="\n")
+
+
+
+
+# s ={1,2,3}
+# print(s.pop())
+
+# list_123 = [1, 2, 3]
+# print(list_123.pop())
+
